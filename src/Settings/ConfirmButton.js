f27633d167b2c326d6573352bbbf697f4aa632ce
Mohamed <malkaisi92@gmail>
2021-10-18 13:03:57 +0100

    Create CoinGrid

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
-
+import { fontSize1,greenBoxShadow,color3 } from '../Shared/Styles';
 export const ConfirmButtonStyled = styled.div`
 margin: 20px;
-color: green;
+color: ${color3}
+${fontSize1}
+padding:5px
 cursor: pointer; 
+&:hover {
+   ${greenBoxShadow}
+}
 `;
 export const CenterDiv = styled.div`
 display:grid;
