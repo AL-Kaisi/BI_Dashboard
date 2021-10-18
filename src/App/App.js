09dfd41f397370e43dfff340dca5a76a8ef260ba
Mohamed <malkaisi92@gmail>
2021-10-18 12:31:45 +0100

    Add loading Content

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,7 @@ import AppLayout  from './AppLayout';
 import AppBar from './AppBar';
 import { AppProvider } from './AppProvider';
 import Settings from '../Settings';
+import Content from '../Shared/Content';
 
 class App extends Component {
   render(){
@@ -16,9 +17,9 @@ class App extends Component {
       <AppLayout>
       <AppProvider>
       <AppBar/>
-
+<Content>
    <Settings/>
-
+   </Content>
    </AppProvider>
    </AppLayout>
     );
