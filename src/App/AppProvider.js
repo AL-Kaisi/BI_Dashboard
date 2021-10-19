55896c7033a170aa191a1b638bec0cc29b537885
Mohamed <malkaisi92@gmail>
2021-10-19 12:56:24 +0100

    Fuzzy Search created

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -17,7 +17,8 @@ export class AppProvider extends React.Component {
             addCoin: this.addCoin,
             removeCoin: this.removeCoin,
             isInFavorites: this.isInFavorites,
-            confirmFavorites: this.confirmFavorites
+            confirmFavorites: this.confirmFavorites,
+            setFilteredCoins: this.setFilteredCoins
 
         }
     }
@@ -68,6 +69,10 @@ export class AppProvider extends React.Component {
         return {favorites};
     }
     setPage = page => this.setState({page});
+
+
+    setFilteredCoins = (filteredCoins) => this.setState({filteredCoins})
+
 
     render(){
         return(
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,3 +1,4 @@
+import { coinList } from "cryptocompare";
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
@@ -10,18 +11,23 @@ grid-template-columns: repeat(auto-fill,minmax(130px,1fr));
 grid-gap: 15px;
 margin-top: 40px;
 `;
-function getCoinsToDisplay(coinList, topSection, favorites){
-    return topSection ? favorites : Object.keys(coinList).slice(0,100);
+
+function getLowerSectionCoins(filteredCoins,coinList){
+return filteredCoins && Object.keys(filteredCoins) ||
+Object.keys(coinList).slice(0,100) 
+}
+function getCoinsToDisplay(coinList, topSection, favorites,filteredCoins){
+    return topSection ? favorites : getLowerSectionCoins(filteredCoins,coinList);
     
   }
   export default function CoinGrid({topSection}){
     return (
       <AppContext.Consumer>
-      {({coinList,favorites}) => {
+      {({coinList,favorites,filteredCoins}) => {
           return (
 
             <CoinGridStyled>
-              {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey} />
+              {getCoinsToDisplay(coinList, topSection, favorites,filteredCoins).map(coinKey => <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey} />
               )}
             </CoinGridStyled>
           );
